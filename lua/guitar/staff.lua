867df4b739da69e7b5c052e9839d3a35a0f4ce14
Mike Mehl <mmehl@cisco.com>
2024-04-30 14:08:19 -0400

    Working on staff stuff.

--- lua/guitar/staff.lua
+++ lua/guitar/staff.lua
@@ -1,29 +1,87 @@
 ---@module 'guitar.staff'
 local staff = {}
 
----@param note string
----@param length number?
----@return string
-function staff.create_string(note, length)
-  local l = length or 80
-  local line = note .. "|"
-  for _ = 1, l, 1 do
-    line = line .. "-"
+local function get_max_tuning_string(tuning)
+  local max = 1
+  for _, n in ipairs(tuning) do
+    if string.len(n) > max then
+      max = string.len(n)
+    end
   end
-  line = line .. "|"
-  return line
+  return max
 end
 
----@param tuning string[]
----@param length number?
----@return string
-function staff.create(tuning, length)
-  local l = length or 80
-  local out = ""
-  for _, n in ipairs(tuning) do
-    out = out .. staff.create_string(n, l) .. "\n"
+local function has_barline(barlines, barline)
+  for _, bl in ipairs(barlines) do
+    if bl == barline then
+      return true
+    end
   end
-  return out
+  return false
+end
+
+---@class Staff
+---@field barlines number[]
+---@field strings string[]
+---@field length number
+---@field private __tune_column_size number
+---@field private __state string
+local Staff = {}
+
+---@return Staff
+---@param length number?
+---@param barlines number[]?
+---@param strings string[]?
+function staff.new(strings, length, barlines)
+  local s = {
+    barlines = barlines or {},
+    strings = strings or {},
+    length = length or 80,
+    __tune_column_size = 1,
+  }
+  if strings then
+    s.__tune_column_size = get_max_tuning_string(strings)
+  end
+  setmetatable(s, { __index = Staff })
+  return s
+end
+
+---@private
+function Staff:generate()
+  local out = ""
+  for _, n in ipairs(self.strings) do
+    out = out .. n
+    while #out < self.__tune_column_size do
+      out = out .. " "
+    end
+    out = out .. "|"
+    for i = 1, self.length, 1 do
+      if has_barline(self.barlines, i) then
+        out = out .. "|"
+      else
+        out = out .. "-"
+      end
+    end
+    out = out .. "|\n"
+  end
+  self.__state = out
+end
+
+---@return boolean
+---@param col number
+function Staff:add_barline(col)
+  if col >= self.length then
+    return false
+  end
+  table.insert(self.barlines, col)
+  table.sort(self.barlines)
+  return true
+end
+
+---@return string
+function Staff:get()
+  self:generate()
+  return self.__state
 end
 
 return staff
--- tests/guitar/staff_spec.lua
+++ tests/guitar/staff_spec.lua
@@ -1,9 +1,10 @@
 local staff = require("guitar.staff")
 
-describe("setup", function()
-  it("returns a tablature line string", function()
-    local expected_string = "E|--------------------------------------------------------------------------------|"
-    assert(staff.create_string("E") == expected_string, "Bad line returned")
+describe("staff", function()
+  local test_staff
+
+  before_each(function()
+    test_staff = staff.new({ "D", "A", "F", "C", "G", "C" })
   end)
 
   it("returns a tablature staff", function()
@@ -14,6 +15,22 @@ C|------------------------------------------------------------------------------
 G|--------------------------------------------------------------------------------|
 C|--------------------------------------------------------------------------------|
 ]]
-    assert(staff.create({ "D", "A", "F", "C", "G", "C" }) == expected_string, "Bad staff returned")
+    assert(test_staff:get() == expected_string, "Bad staff returned")
+  end)
+
+  it("can add barlines", function()
+    assert(test_staff:add_barline(12) == true, "Add barline returned false")
+    local expected_string = [[D|-----------|--------------------------------------------------------------------|
+A|-----------|--------------------------------------------------------------------|
+F|-----------|--------------------------------------------------------------------|
+C|-----------|--------------------------------------------------------------------|
+G|-----------|--------------------------------------------------------------------|
+C|-----------|--------------------------------------------------------------------|
+]]
+    assert(test_staff:get() == expected_string, "Bad staff returned")
+  end)
+
+  it("won't add barlines longer than the staff", function()
+    assert(test_staff:add_barline(1024) == false, "Add barline returned true for out of range column")
   end)
 end)
