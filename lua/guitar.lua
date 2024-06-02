9ac9fc5f558355fc94d4d5dd20b659157f2cb945
Mike Mehl <michaelalanmehl@gmail.com>
2024-06-02 14:29:48 -0400

    Test setup, basic test

--- lua/guitar.lua
+++ lua/guitar.lua
@@ -1,16 +1,24 @@
 ---@module 'guitar'
 ---@class Guitar
 local guitar = {}
-local config = require("guitar.config")
 
----@param args GuitarConfig
-function guitar.setup(args)
-  config.set(args)
+function guitar.setup(_)
+  --  TODO: Config and such
 end
 
----@return string
-function guitar.hello()
-  return config.get().msg
+---comment
+---@param win number
+---@param pos [ number, number ]
+---@return boolean?, string?
+function guitar.add_staff(win_in, pos_in)
+  local win = win_in or vim.api.nvim_get_current_win()
+  assert(win)
+  local pos = pos_in or vim.api.nvim_win_get_cursor(win)
+  assert(pos)
+  assert(#pos == 2)
+  assert(type(pos[1]) == "number")
+  assert(type(pos[2]) == "number")
+  return true, nil
 end
 
 return guitar
--- test_shim.sh
+++ test_shim.sh
@@ -3,7 +3,5 @@ set -euo
 if [[ ${TRACE-0} -eq 1 ]]; then set -x; fi;
 eval "$(luarocks path)"
 nvim --clean \
-  --cmd "set runtimepath $(pwd -P)" \
-  --cmd "lua package.path=package.path .. \"$(pwd -P)/lua/?.lua\"" \
   -l lua_modules/lib/luarocks/rocks-5.1/busted/2.2.0-1/bin/busted
 
