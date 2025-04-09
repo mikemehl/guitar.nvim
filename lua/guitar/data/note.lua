82541aeebbb976ef21f0ed14fe91384ee720997f
Mike Mehl <michaelalanmehl@gmail.com>
2025-04-09 13:54:13 -0400

    Hmmmmmm

--- lua/guitar/data/note.lua
+++ lua/guitar/data/note.lua
@@ -6,4 +6,18 @@ local Note = {}
 
 
 
+function Note.new(f, s, l, o, r)
+   return {
+      fret = f,
+      string = s,
+      length = l,
+      offset = o,
+      rest = r,
+   }
+end
+
+function Note:isRest()
+   return self.rest
+end
+
 return Note
