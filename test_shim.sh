c7b33ffcac0eba16e02c9e5d5c7bdec3795fe3d4
Mike Mehl <michaelalanmehl@gmail.com>
2025-04-09 12:54:28 -0400

    Reworking.

--- test_shim.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo
-if [[ ${TRACE-0} -eq 1 ]]; then set -x; fi;
-eval "$(luarocks path)"
-nvim --clean \
-  -l lua_modules/lib/luarocks/rocks-5.1/busted/2.2.0-1/bin/busted
-
